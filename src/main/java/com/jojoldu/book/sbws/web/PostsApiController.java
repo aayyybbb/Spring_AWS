4867c963de7dc4b2f6e455bd26ab3c2d372a449e
aayyybbb <yb226@kakao.com>
2024-08-09 16:12:10 +0900

    add insert api & test code

---
+++ src/main/java/com/jojoldu/book/sbws/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.jojoldu.book.sbws.web;
+
+import com.jojoldu.book.sbws.service.PostsService;
+import com.jojoldu.book.sbws.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
