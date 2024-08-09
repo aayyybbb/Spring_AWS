77182153f4a7d3be745f7e4133ccfb74ab526609
aayyybbb <yb226@kakao.com>
2024-08-09 16:52:43 +0900

    add update&read api, test code

--- src/test/java/com/jojoldu/book/sbws/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/sbws/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.sbws.web;
 import com.jojoldu.book.sbws.domain.posts.Posts;
 import com.jojoldu.book.sbws.domain.posts.PostsRepository;
 import com.jojoldu.book.sbws.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.sbws.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -50,5 +53,31 @@ public class PostsApiControllerTest {
         List<Posts> posts = postsRepository.findAll();
         assertThat(posts.get(0).getTitle()).isEqualTo(title);
         assertThat(posts.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void postsUpdate() throws Exception {
+        Posts postsSave = postsRepository.save(Posts.builder().title("title").content("content").build());
+
+        Long updateId = postsSave.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto =
+                PostsUpdateRequestDto.builder().title(expectedTitle).content(expectedContent).build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> request = new HttpEntity<>(requestDto);
+
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT,request, Long.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> posts = postsRepository.findAll();
+        assertThat(posts.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(posts.get(0).getContent()).isEqualTo(expectedContent);
+
+
     }
 }
